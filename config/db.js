a9d22526d78ea7aecddb133b6be962d08b1dadb0
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-01-28 17:49:26 +0500

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
   try {
-    const conn = await mongoose.connect()
+    const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.error('MongoDB connection failed:', error.message)
