0893b05e1d1d08f76bd725093f72593eea77ee40
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-01-31 06:00:19 +0500

    update

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const PORT = process.env.PORT || 5000
 // ===*MIDDLEWARE*===
 app.use(express.json())
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173', // Vite frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  }),
-)
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173', // Vite frontend
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//   }),
+// )
 
 // ===*CONNECT DATABASE*===
 connectDB()
